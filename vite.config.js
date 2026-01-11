4eed99232fcbc28d5e259369da74b05783190535
JoshTsang916 <bonkerser@gmail.com>
2026-01-11 14:39:20 +0800

    設定檔案

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // 設定相對路徑，讓部署到任何子目錄都能正常運作
 })
